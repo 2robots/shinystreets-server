adf69651316b372dc00582bc3c22488205795420
Tina <kristina.heidinger@gmail.com>
2013-08-26 08:11:56 +0200

    enabled admin interface

--- shinystreets/shinystreets/urls.py
+++ shinystreets/shinystreets/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('shinystreetsapp.views',
     url(r'^areas/$', 'area_list'),
     url(r'^areas/(?P<pk>[0-9]+)/$', 'area_detail'),
+
+    #admin interface
+    url(r'^admin/', include(admin.site.urls)),
 )
